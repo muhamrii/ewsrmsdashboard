c5917640e5ba8993abf5c4fc49686d882fc6f2fb
muhamrii <muhamri1998@gmail.com>
2020-11-22 18:55:33 +0700

    addrealtimedetailv1

--- ewsrmsdashboard/rmsdash/urls.py
+++ ewsrmsdashboard/rmsdash/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-    path('realtimedashboard', views.realtime, name='realtime')
+    path('realtimedashboard', views.realtime, name='realtime'),
     path('realtime-detail/<str:servername>', views.realtimedetail, name='realtimedetail')
 
 ]
