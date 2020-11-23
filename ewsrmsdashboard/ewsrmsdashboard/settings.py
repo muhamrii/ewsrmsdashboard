911a0ce6dd2d3f229d63bdd2b213be95d2d21861
muhamrii <muhamri1998@gmail.com>
2020-11-23 16:48:17 +0700

    costumizetemplaterealtimedesign

--- ewsrmsdashboard/ewsrmsdashboard/settings.py
+++ ewsrmsdashboard/ewsrmsdashboard/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_plotly_dash.apps.DjangoPlotlyDashConfig',
-    'channels',
     'channels_redis'
 ]
 
