979727c493a4b68e308827b6bd3aeec5236acddd
muhamrii <muhamri1998@gmail.com>
2020-11-23 11:58:38 +0700

    addplotramv9

--- ewsrmsdashboard/rmsdash/views.py
+++ ewsrmsdashboard/rmsdash/views.py
@@ -55,8 +55,7 @@ def realtimedetail(request, servername):
     def plotram(filterserver):
         db_connection = sql.connect(host='localhost', database='db_ewsrmsdash', user='root', password='Last_12321', auth_plugin='mysql_native_password')
         df = pd.read_sql("select timeid, servername, memload,cpuload, sshstatus from tb_cpu_ram_load where timeid > now() - INTERVAL 24 HOUR;", con=db_connection)
-        filter1 = df["servername"]=filterserver
-        df = df.where(filter1, inplace = True)
+        df = df.loc[df['servername'] == filterserver]
         fig = px.line(df, x="timeid", y="memload", title=filterserver)
         plot_div = plot(fig, output_type='div', include_plotlyjs=False)
         return plot_div
