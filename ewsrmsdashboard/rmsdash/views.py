bbe65ace9c43f4b81354145bb7b06c76b704334b
muhamrii <muhamri1998@gmail.com>
2020-11-23 11:02:57 +0700

    addplotramv6

--- ewsrmsdashboard/rmsdash/views.py
+++ ewsrmsdashboard/rmsdash/views.py
@@ -56,7 +56,7 @@ def realtimedetail(request, servername):
         db_connection = sql.connect(host='localhost', database='db_ewsrmsdash', user='root', password='Last_12321', auth_plugin='mysql_native_password')
         df = pd.read_sql("select timeid, servername, memload,cpuload, sshstatus from tb_cpu_ram_load where timeid > now() - INTERVAL 24 HOUR;", con=db_connection)
 
-        fig = px.line(df, x="timeid", y="memload", color="servername", title='{filterserver}')
+        fig = px.line(df, x="timeid", y="memload", color="servername", title=filterserver)
         plot_div = plot(fig, output_type='div', include_plotlyjs=False)
         return plot_div
 
